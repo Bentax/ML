4bf0815d15c71deb390cf89fb70d237aec1f30d4
Andrey Kuzin <bentaxrus@mail.ru>
2024-05-30 11:52:18 +0300

    Create get_mae.py

---
+++ get_mae.py
@@ -0,0 +1,19 @@
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+# The data is loaded into train_X, val_X, train_y and val_y using the code you've already seen and which you've already written
+
+# We can use a for-loop to compare the accuracy of models built with different values for max_leaf_nodes
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+
