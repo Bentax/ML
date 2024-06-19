b3d5973c9cc34e28bd2079a0888b23943aa12473
Andrey Kuzin <bentaxrus@mail.ru>
2024-06-19 17:49:53 +0300

    Create N_grams.py

---
+++ N_grams.py
@@ -0,0 +1,7 @@
+from nltk.util import ngrams, bigrams
+ste = list(bigrams(["я", "з", "ы", "к" ]))
+pik = list(ngrams(["я", "з", "ы", "к" ], 3))  
+print(ste)
+print(pik)
+######## [('я', 'з'), ('з', 'ы'), ('ы', 'к')]
+[('я', 'з', 'ы'), ('з', 'ы', 'к')]
